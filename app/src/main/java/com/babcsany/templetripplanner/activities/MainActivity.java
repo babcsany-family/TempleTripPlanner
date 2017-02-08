d1c7b9322cb3e4ed643b465e765b13cf278990b6
Peter Babcsany <peter@babcsany.com>
2017-02-08 23:48:39 +0100

    added navigation drawer and list fragment

--- app/src/main/java/com/babcsany/templetripplanner/activities/MainActivity.java
+++ app/src/main/java/com/babcsany/templetripplanner/activities/MainActivity.java
@@ -6,6 +6,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -16,10 +20,10 @@ import android.widget.EditText;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import com.babcsany.templetripplanner.interfaces.ITempleHostelPricingCalculator;
-import com.babcsany.templetripplanner.adapters.PatronAdapter;
 import com.babcsany.templetripplanner.R;
+import com.babcsany.templetripplanner.adapters.PatronAdapter;
 import com.babcsany.templetripplanner.enums.PatronKind;
+import com.babcsany.templetripplanner.interfaces.ITempleHostelPricingCalculator;
 import com.babcsany.templetripplanner.parcels.Patron;
 import org.parceler.Parcels;
 
@@ -28,14 +32,16 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.ListIterator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String NAME_IN_EMAIL_SIGNATURE = "name_in_email_signature";
     private static final int ADD_PATRON_REQUEST = 2;
 
     @BindView(R.id.toolbar) Toolbar toolbar;
-//    @BindView(R.id.fragment) MainActivityFragment fragment;
+    //    @BindView(R.id.fragment) MainActivityFragment fragment;
     @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.nav_view) NavigationView navigationView;
+    @BindView(R.id.drawer_layout) DrawerLayout drawer;
 
     private RecyclerView patronsListView;
     private ITempleHostelPricingCalculator selectedTemple = new FreibergTemplePricingCalculator();
@@ -60,8 +66,25 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         patronsListView = (RecyclerView) findViewById(R.id.patronsListView);
 //        fragment.setTempleHostelPricingCalculator(selectedTemple);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -107,6 +130,29 @@ public class MainActivity extends AppCompatActivity {
     public void addNewPatron(MenuItem item) {
         Intent intent = new Intent(this, PatronActivity.class);
         startActivityForResult(intent, ADD_PATRON_REQUEST);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.my_trips) {
+            // Handle the camera action
+        } else if (id == R.id.find_seats) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @OnClick(R.id.fab)
